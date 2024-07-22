f283cec91c9ac6a5ee7e718b0b60504415545727
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-23 00:22:24 +0300

    feat: move xdg to home-manager

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ config, ...}: {
+{ config, pkgs, ...}: {
   home.username = "korsilyn";
   home.homeDirectory = "/home/korsilyn";
   programs.home-manager.enable = true;
@@ -6,5 +6,35 @@
     #./apps
     ./dotfiles
   ];
+
+  xdg = {
+    enable = true;
+    mime.enable = true;
+    mimeApps.enable = true;
+    portal = {
+      enable = true;
+      xdgOpenPortal = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+      config = {
+        sway = {
+          default = [
+            "wlr"
+          ];
+          "org.freedesktop.impl.portal.Settings" = [
+            "wlr"
+            "gtk"
+          ];
+        };
+      };
+    };
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+
   home.stateVersion = "24.05";
 }

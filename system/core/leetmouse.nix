bc7feda51273c0ebeaf8284e599e648d677fc30b
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-23 17:53:45 +0300

    leetmouse udev update

--- system/core/leetmouse.nix
+++ system/core/leetmouse.nix
@@ -6,6 +6,6 @@ in {
   services.udev.extraRules = ''
     ACTION=="bind", SUBSYSTEMS=="usb|input|hid", ID_INPUT_MOUSE=="1", \
     RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/hid/drivers/hid-generic/unbind'", \
-    RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/hid/drivers/leetmouse/bind'" 
+    RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/usb/drivers/leetmouse/bind'" 
   '';
 }

c2807c0404cd67c34711b77b1b248db7c252c670
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-24 16:21:27 +0300

    chore: bump flake, add swap

--- hosts/alhena/hardware-configuration-fs.nix
+++ hosts/alhena/hardware-configuration-fs.nix
@@ -43,7 +43,10 @@
       options = [ "subvol=nix" "compress-force=zstd" "noatime" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+    device = "/.swapvol/swapfile";
+    size = 16 * 1024;
+  } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

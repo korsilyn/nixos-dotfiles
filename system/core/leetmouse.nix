1d07e2b7044707c3708e55958b99a21058cf2206
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-24 16:21:27 +0300

    bump flake, add swap

--- system/core/leetmouse.nix
+++ system/core/leetmouse.nix
@@ -4,7 +4,7 @@ let
 in {
   boot.extraModulePackages = [ leetmouse ];
   services.udev.extraRules = ''
-    ACTION=="bind", SUBSYSTEMS=="usb|input|hid", ID_INPUT_MOUSE=="1", \
+    ACTION=="add|change", SUBSYSTEMS=="usb|input|hid", ENV{ID_INPUT_MOUSE}=="1", \
     RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/hid/drivers/hid-generic/unbind'", \
     RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/usb/drivers/leetmouse/bind'" 
   '';

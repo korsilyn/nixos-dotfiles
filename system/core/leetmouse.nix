40b81467fa344f857a28e43d8807cabfedf46e2c
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-24 16:26:30 +0300

    fix: change leetmouse

--- system/core/leetmouse.nix
+++ system/core/leetmouse.nix
@@ -3,8 +3,9 @@ let
   leetmouse = config.boot.kernelPackages.callPackage ../../overlays/leetmouse-kernel.nix {};
 in {
   boot.extraModulePackages = [ leetmouse ];
+  boot.kernelModules = [ "leetmouse" ];
   services.udev.extraRules = ''
-    ACTION=="add|change", SUBSYSTEMS=="usb|input|hid", ENV{ID_INPUT_MOUSE}=="1", \
+    ACTION=="add", SUBSYSTEM=="input", KERNEL=="mouse[0-9]", \
     RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/hid/drivers/hid-generic/unbind'", \
     RUN+="${pkgs.runtimeShell} -c 'echo $kernel > /sys/bus/usb/drivers/leetmouse/bind'" 
   '';

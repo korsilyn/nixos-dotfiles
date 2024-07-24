0f3b6b610bc07d3ee5a326a01bbde188333ab8e4
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-24 14:49:51 +0300

    revert: c6e2ce2

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./direnv.nix
     ./nixvim.nix
   ];
 }
--- system/core/users.nix
+++ system/core/users.nix
@@ -8,7 +8,6 @@
   #];
 
   programs.zsh.enable = true;
-  programs.direnv.enable = true;
   users.users = {
     korsilyn = {
       isNormalUser = true;

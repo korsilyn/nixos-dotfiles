410f4ac99411bf4834cdbeecc93750dc877b6a23
Eugene Gladkov <jenya.love72@gmail.com>
2024-07-25 12:30:48 +0300

    Update flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -2,94 +2,143 @@
   description = "NixOS configuration by korsilyn";
 
   inputs = {
+    # global, so they can be `.follow`ed
+    systems.url = "github:nix-systems/default-linux";
+
+    # Unstable, because I want rolling release
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-unstable-small"; # moves faster, has less packages
 
-    # Formatters
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    # Powered by
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
-    # Pre-commit hooks
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+    # Flake Utils for follows
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
     };
 
-    # Wallpapers to catppuchin theme
-    catppuccinifier = {
-      url = "github:lighttigerXIV/catppuccinifier";
-      inputs.nixpkgs.follows = "nixpkgs";
+    # Same with Flake Compat
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    # Same with NixPak
+    nixpak = {
+      url = "github:nixpak/nixpak";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-small";
+        flake-parts.follows = "flake-parts";
+      };
+    };
+
+    # Repo for hardware-specific NixOS modules
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    # Nix wrapper
+    nh = {
+      url = "github:viperML/nh";
+      inputs.nixpkgs.follows = "nixpkgs-small";
+    };
+
+    # Tree-wide formatter
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs-small";
+    };
+
+    # Nix formatter
+    nixfmt = {
+      url = "github:nixos/nixfmt";
+      flake = false;
+    };
+
+    # Git hooks
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-small";
+        flake-compat.follows = "flake-compat";
+      };
+    };
+
+    # nix-index database
+    nix-index-db = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs-small";
+    };
+
+    # Secrets management
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-small";
+        home-manager.follows = "home-manager";
+        darwin.follows = "";
+      };
+    };
+
+    # Rust overlay
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-small";
+      };
+    };
+
+    # Nix Language server
+    nil = {
+      url = "github:oxalica/nil";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-small";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    # An extensible Neovim configuration wrapper.
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nil.follows = "nil";
+        flake-utils.follows = "flake-utils";
+        flake-parts.follows = "flake-parts";
+        systems.follows = "systems";
+      };
+    };
+
+    # Schizophrenic Firefox configuration
+    schizofox = {
+      url = "github:schizofox/schizofox";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-small";
+        flake-parts.follows = "flake-parts";
+        flake-compat.follows = "flake-compat";
+        home-manager.follows = "home-manager";
+        nixpak.follows = "nixpak";
+        systems.follows = "systems";
+      };
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-parts,
-    ...
-  } @inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
-      systems = [
-        "x86_64-linux"
-      ];
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
 
       imports = [
-        {
-          config._module.args._inputs = inputs // {inherit (inputs) self;};
-        }
-
-        #inputs.flake-parts.flakeModules.easyOverlay
-        inputs.pre-commit-hooks.flakeModule
-        inputs.treefmt-nix.flakeModule
+        ./parts
+        ./hosts
       ];
-
-      perSystem = {
-        inputs',
-        config,
-        pkgs,
-        ...
-      }: {
-        pre-commit = {
-          settings.excludes = ["flake.lock"];
-          settings.hooks = {
-            alejandra.enable = true;
-            prettier.enable = true;
-          };
-        };
-
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs = {
-            alejandra.enable = true;
-            black.enable = true;
-            deadnix.enable = true;
-            shellcheck.enable = true;
-            shfmt = {
-              enable = true;
-              indent_size = 4;
-            };
-          };
-        };
-      };
-
-      flake = {
-        nixosConfigurations = import ./hosts inputs;
-      };
-    });
+    };
 }

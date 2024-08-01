3ecf01bfa158298feddd5a498bc0ceeb6ceb74c1
Eugene Gladkov <jenya.love72@gmail.com>
2024-08-01 14:16:14 +0300

    fs fix

--- hosts/alhena/hardware-configuration-fs.nix
+++ hosts/alhena/hardware-configuration-fs.nix
@@ -14,31 +14,31 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/074272bf-559c-4348-80cb-8c3814a91c38";
+    { device = "/dev/disk/by-label/NIXROOT";
       fsType = "btrfs";
       options = [ "subvol=rootfs" "compress-force=zstd" "noatime" ];
     };
 
   fileSystems."/.swapvol" =
-    { device = "/dev/disk/by-uuid/074272bf-559c-4348-80cb-8c3814a91c38";
+    { device = "/dev/disk/by-label/NIXROOT";
       fsType = "btrfs";
       options = [ "subvol=swap" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3829-DD9F";
+    { device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/074272bf-559c-4348-80cb-8c3814a91c38";
+    { device = "/dev/disk/by-label/NIXROOT";
       fsType = "btrfs";
       options = [ "subvol=home" "compress-force=zstd" "noatime" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/074272bf-559c-4348-80cb-8c3814a91c38";
+    { device = "/dev/disk/by-label/NIXROOT";
       fsType = "btrfs";
       options = [ "subvol=nix" "compress-force=zstd" "noatime" ];
     };
